0f6233706a97ff1597d63d3ed45dc76812eabfae
Suryansh Singh <singhsuryansh2746@gmail.com>
2026-08-17 21:37:48 +0530

    routes

--- todoflow/routes/web.php
+++ todoflow/routes/web.php
@@ -1,7 +1,43 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\DashboardController;
+
+
+Route::get('/login', [AuthController::class, 'showLogin'])
+    ->name('login');
+
+Route::post('/login', [AuthController::class, 'login']);
+
+
+Route::get('/signup', [AuthController::class, 'showSignup'])
+    ->name('signup');
+
+Route::post('/signup', [AuthController::class, 'signup']);
+
+
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->name('logout');
+
+
+
+Route::get('/dashboard', [DashboardController::class, 'index'])
+    ->middleware('auth')
+    ->name('dashboard');
+
+
+Route::middleware('auth')->group(function () {
+
+    Route::resource('tasks', TaskController::class);
+
+});
+
+
 
 Route::get('/', function () {
-    return view('welcome');
+
+    return redirect('/dashboard');
+
 });
