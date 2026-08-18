5f9ac934bf0224b93d486dad27981613a7ebd52c
Suryansh Singh <singhsuryansh2746@gmail.com>
2026-08-18 18:56:25 +0530

    added authroute to login/signup

--- todoflow/routes/web.php
+++ todoflow/routes/web.php
@@ -6,34 +6,32 @@ use App\Http\Controllers\TaskController;
 use App\Http\Controllers\DashboardController;
 
 
-Route::get('/login', [AuthController::class, 'showLogin'])
-    ->name('login');
+Route::middleware('guest')->group(function () {
 
-Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/login', [AuthController::class, 'showLogin'])
+        ->name('login');
 
+    Route::post('/login', [AuthController::class, 'login']);
 
-Route::get('/signup', [AuthController::class, 'showSignup'])
-    ->name('signup');
+    Route::get('/signup', [AuthController::class, 'showSignup'])
+        ->name('signup');
 
-Route::post('/signup', [AuthController::class, 'signup']);
+    Route::post('/signup', [AuthController::class, 'signup']);
 
-
-Route::post('/logout', [AuthController::class, 'logout'])
-    ->name('logout');
-
-
-
-Route::get('/dashboard', [DashboardController::class, 'index'])
-    ->middleware('auth')
-    ->name('dashboard');
+});
 
 
 Route::middleware('auth')->group(function () {
 
+    Route::post('/logout', [AuthController::class, 'logout'])
+        ->name('logout');
+
+    Route::get('/dashboard', [DashboardController::class, 'index'])
+        ->name('dashboard');
+
     Route::resource('tasks', TaskController::class);
 
 });
-
 
 
 Route::get('/', function () {
